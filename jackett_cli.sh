6eb16e07b3b0cacf6182d08bc159e1ed671dd02a
A1337Xyz <blindwizard@tutanota.com>
2023-06-30 13:28:21 -0300

    2023.06.30

--- jackett_cli.sh
+++ jackett_cli.sh
@@ -12,11 +12,13 @@ declare -r -x FILE=/tmp/jackett_cli.$$.json
 declare -r FZF_DEFAULT_OPTS="--multi --exact --no-separator --cycle --no-hscroll --no-scrollbar --color=dark --no-border --no-sort --tac --listen 1337"
 declare -x filter=all
 
-trap 'rm $FILE ${FILE}.mode 2>/dev/null || true' EXIT
+trap 'rm $FILE ${FILE}.curr ${FILE}.prev 2>/dev/null || true' EXIT
 
 help() {
     cat << EOF
 Usage: ${0##*/} [option] <query>
+
+-h --help               Show this message and exit
 
 -f --filter  FILTER     Supported filters
                             type:<type>
@@ -50,6 +52,8 @@ done
 fzf_cmd() { curl -s -XPOST localhost:1337 -d "$1" >/dev/null 2>&1 || true; }
 
 main() {
+    prev=${FILE}.prev
+    curr=${FILE}.curr
     case "$1" in
         download)
             shift
@@ -63,30 +67,36 @@ main() {
             ;;
         menu)
             fzf_cmd "change-prompt(Menu: )"
-            for i in Title Seeders Peers Grabs Size CategoryDesc Tracker PublishDate; do echo sort:$i ;done
-            printf 'select:Category\n'
+            for i in Title Seeders Peers Grabs Size Tracker;do echo "$i:Sort by $i" ;done
+            echo 'PublishDate:Sort by Date'
+            echo 'CategoryDesc:Sort by Category'
+            echo 'CategoryDesc:Category'
+            echo 'TrackerType:Type'
             ;;
-        select:Category)
-            jq -Mcr '.Results[].CategoryDesc | "cat:\(.)"' "$FILE" | sort -u
-            ;;
-        cat:*)
-            v=${1/cat:}
-            fzf_cmd "change-prompt(($v) Search: )"
-            jq -Mcr --arg v "$v" '.Results | to_entries[] | select(.value.CategoryDesc == $v) | "\(.key):\(.value.Title)"' "$FILE"
-            ;;
-        sort:*) 
-            k=${1/sort:/}
+        [A-Z]*:Sort*) 
+            k=${1%%:*}
             fzf_cmd "change-prompt((Sorting by ${k}) Search: )"
-            jq -Mcr --arg k "$k" '.Results as $r | $r | [to_entries[] | {k: .key, v: .value[$k]}] | sort_by(.v)[] | "\(.k):\($r[.k].Title)"' "$FILE"
+            grep -xFf "$prev" < <(jq -Mcr --arg k "$k" \
+                '.Results as $r | $r | [to_entries[] | {k: .key, v: .value[$k]}] | sort_by(.v)[] | "\(.k):\($r[.k].Title)"' "$FILE") | tee "$curr"
+            ;;
+        [A-Z]*:Category|[A-Z]*:Type)
+            k=${1%%:*}
+            jq -Mcr --arg k "$k" '.Results[][$k] | "\($k):\(.)"' "$FILE" | sort -u
+            ;;
+        [A-Z]*:*)
+            k=${1%%:*} v=${1##*:}
+            fzf_cmd "change-prompt(($v) Search: )"
+            jq -Mcr --arg k "$k" --arg v "$v" '.Results | to_entries[] | select(.value[$k] == $v) | "\(.key):\(.value.Title)"' "$FILE" | tee "$curr"
             ;;
         *)
             query=${2:-$1}
             fzf_cmd "change-prompt(Searching... )"
             curl -s "${API_URL}/${filter:-all}/results?apikey=${API_KEY}&Query=${query// /+}" -o "$FILE"
-            jq -Mcr '.Results | to_entries[] | "\(.key):\(.value.Title)"' "$FILE"
+            jq -Mcr '.Results | to_entries[] | "\(.key):\(.value.Title)"' "$FILE" | tee "$curr"
             fzf_cmd 'change-prompt(Search: )'
             ;;
     esac
+    cp "$curr" "$prev"
 }
 
 preview() {
